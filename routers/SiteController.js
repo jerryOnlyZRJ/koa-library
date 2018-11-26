930c2ddecc7cb2d288d3dad8c7c8fd98db0ab399
ranjayzheng <ranjayzheng@qq.com>
2018-11-26 21:43:45 +0800

    finish create page ssr

--- routers/SiteController.js
+++ routers/SiteController.js
@@ -2,30 +2,39 @@ const Router = require('koa-router')
 const BookModel = require('../models/Book')
 const BookController = require("./BookController")
 
+const fields = ["name", "author", "date", "score"]
+
 const router = new Router()
 // 串联BookController api子路由
 router.use('/api', BookController.routes(), BookController.allowedMethods());
 const bookModel = new BookModel()
 
 //配置根路由
-router.get('/', async(ctx, next) => {
+router.get('/', async (ctx, next) => {
     // ctx.router available
     ctx.body = await ctx.render('index', {
-    	data: await bookModel.actionIndex()
+        data: await bookModel.actionIndex()
     })
 });
 
 //配置拓展路由
-router.get('/view', async(ctx, next) => {
-	ctx.body = await ctx.render('view', {
-    	data: await bookModel.actionView(ctx.query.id)
+router.get('/create', async (ctx, next) => {
+    ctx.body = await ctx.render('create', {
+        fields
+    })
+})
+
+//配置拓展路由
+router.get('/view', async (ctx, next) => {
+    ctx.body = await ctx.render('view', {
+        data: await bookModel.actionView(ctx.query.id)
     })
 })
 
 //配置与models配合的数据路由
-router.get('/update', async(ctx, next) => {
-	ctx.body = await ctx.render('update', {
-    	data: await bookModel.actionView(ctx.query.id)
+router.get('/update', async (ctx, next) => {
+    ctx.body = await ctx.render('update', {
+        data: await bookModel.actionView(ctx.query.id)
     })
 })
 
