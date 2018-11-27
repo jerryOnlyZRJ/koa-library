f73348d9598d5066939821b7cc2af34277387e31
ranjayzheng <ranjayzheng@qq.com>
2018-11-27 17:14:33 +0800

    finish e2e test

---
+++ tests/e2e.js
@@ -0,0 +1,35 @@
+const Rize = require('rize')
+const rize = new Rize({
+    headless: false
+})
+// test view logic
+rize
+    .goto('http://localhost:3000/')
+    .click('tr[data-key="item.id"]:first-child .glyphicon')
+    .waitForNavigation()
+    .assertTitle("View")
+    .assertSeeIn('.country-view h1', '遮蔽的天空')
+//test create&update&delete logic
+rize
+    .goto('http://localhost:3000/')
+    .click('.btn.btn-success')
+    .waitForNavigation()
+    .assertTitle("Create")
+    .type('.form-group input[name="name"]', 'test')
+    .type('.form-group input[name="author"]', 'test')
+    .type('.form-group input[name="date"]', 'date')
+    .type('.form-group input[name="score"]', '8')
+    .click('.btn.btn-success')
+    .waitForNavigation()
+    .assertSee('test')
+    .click('tr[data-key="item.id"]:last-child .glyphicon-pencil')
+    .waitForNavigation()
+    .assertTitle("Update")
+    .assertSeeIn('.country-update h1', 'Update Book: test')
+    .clear('.form-group input[name="name"]')
+    .type('.form-group input[name="name"]', 'test2')
+    .click('.btn.btn-success')
+    .waitForNavigation()
+    .assertSee('test2')
+    .click('tr[data-key="item.id"]:last-child .glyphicon-trash')
+    .end()
