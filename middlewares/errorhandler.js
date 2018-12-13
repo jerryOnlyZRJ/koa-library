190eb7f28609be5929efa18954e48ae129ab607b
ranjayzheng <ranjayzheng@qq.com>
2018-12-13 19:29:20 +0800

    optimize error handler

--- middlewares/errorhandler.js
+++ middlewares/errorhandler.js
@@ -2,8 +2,8 @@
  * @description 容错机制中间件
  * @author Jerry
  */
-const log4js = require("./log4js.config.js");
-const logger = log4js.getLogger("cheese");
+const log4js = require('./log4js.config.js')
+const logger = log4js.getLogger('mix')
 
 /**
  * 容错处理对象
@@ -14,24 +14,24 @@ const errorHandler = {
    * 错误处理句柄
    * @param  {Object} app koa2上下文
    */
-  error(app) {
+  error (app) {
     // 配合中间件迭代器进行容错处理
     app.use(async (ctx, next) => {
       try {
-        await next();
+        await next()
       } catch (err) {
         // node错误日志
-        logger.error(err);
-        ctx.status = err.status || 500;
-        ctx.body = 500;
+        logger.error(err)
+        ctx.status = err.status || 500
+        ctx.body = 500
       }
-    });
+    })
     app.use(async (ctx, next) => {
-      await next();
-      if (ctx.status !== 404) return;
-      ctx.status = 404;
-      ctx.body = await ctx.render("404");
-    });
+      await next()
+      if (ctx.status !== 404) return
+      ctx.status = 404
+      ctx.body = await ctx.render('404')
+    })
   }
-};
-module.exports = errorHandler;
+}
+module.exports = errorHandler
--- middlewares/log4js.config.js
+++ middlewares/log4js.config.js
@@ -1,19 +1,30 @@
-const log4js = require('log4js') 
-const path = require("path")
+const log4js = require('log4js')
+const path = require('path')
 
 log4js.configure({
-	appenders: {
-		cheese: {
-			type: 'file',
-			filename: path.join(__dirname, '../logs/jwslog.log')
-		}
-	},
-	categories: {
-		default: {
-			appenders: ['cheese'],
-			level: 'error'
-		}
-	}
+  appenders: {
+    cheese: {
+      type: 'file',
+      filename: path.join(__dirname, '../logs/jwslog.log')
+    },
+    out: {
+      type: 'stdout',
+      layout: {
+        type: 'basic'
+      }
+    }
+  },
+  categories: {
+    default: {
+      appenders: ['out'],
+      level: 'error'
+    },
+    mix: {
+      appenders: ['cheese', 'out'],
+      level: 'error'
+    }
+  },
+  pm2: true // 如果使用 pm2 -i 方式启动的 node 进程需要设置次为 true
 })
 
 module.exports = log4js
